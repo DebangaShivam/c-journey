49b65b3e8ad9456e5af6d68f7a45e5061c67b887
DebangaShivam <shivam89@zoho.com>
2025-09-27 23:17:53 +0530

    nest

---
+++ nest.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+ int marks ;
+ printf("Enter marks: ");   //marks apss fail over100
+    scanf("%d", &marks);
+
+
+    if(marks >=0 && marks <= 30) {
+        printf("fail \n");
+    }
+    
+    else if (marks >30 && marks <=100){
+        printf("pass \n");
+    }
+
+    else { 
+        printf("over100 \n");
+    }
+
+
+return 0;       
+    
+}
