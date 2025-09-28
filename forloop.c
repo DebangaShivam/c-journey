992e90ba092b9ac310c3e43c91c6f4fb41886994
DebangaShivam <shivam89@zoho.com>
2025-09-29 04:05:11 +0530

    Forloops

---
+++ forloop.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main() {
+     //i is iterator//
+
+
+    for (int i=0; i<=10; i++) {
+        printf("%d \n", i);
+    }     
+    
+    for (char ch='a'; ch<='z'; ch++){
+        printf("%c \n",ch);
+    }
+//infiniuteloops,,,no condition statement//
+
+
+//for (int i=0; ;i++){
+   // printf("%d \n",i);
+//}
+//
+    return 0; 
+
+}
